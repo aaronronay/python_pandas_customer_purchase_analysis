25587aaafa840f3020481abc44d127dbc07c9527
aaronronay <30646034+aaronronay@users.noreply.github.com>
2023-07-19 15:40:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,54 +1,97 @@
 import pandas as pd
 
+def read_purchase_data(file_path):
+    return pd.read_csv(file_path)
+
+def get_num_of_players(data_frame):
+    return data_frame['SN'].nunique()
+
+def create_summary_data_frame(data_frame):
+    average_price = data_frame['Price'].mean()
+    unique_items_count = data_frame['Item Name'].nunique()
+    return pd.DataFrame({
+        'average price': [average_price],
+        'number of unique items': [unique_items_count]
+    })
+
+def calculate_gender_demographics(data_frame):
+    gender_counts = data_frame['Gender'].value_counts()
+    total_gender_count = gender_counts.sum()
+    percentage_of_players = gender_counts.div(total_gender_count) * 100
+    return pd.DataFrame({
+        'total count': gender_counts,
+        'percentage of players': percentage_of_players
+    }).sort_index()
+
+def calculate_age_demographics(data_frame, bins, labels, num_of_players):
+    age_group_counts = data_frame.drop_duplicates(subset='SN').groupby(
+        pd.cut(data_frame['Age'], bins=bins, labels=labels)
+    )['SN'].size().reset_index(name='Total_Count')
+    age_group_counts['Percentage_of_Players'] = age_group_counts['Total_Count'] / num_of_players * 100
+    return age_group_counts.copy()
+
+def perform_gender_purchasing_analysis(data_frame):
+    return data_frame.groupby('Gender').agg(
+        purchase_count=('SN', 'count'),
+        average_purchase_price=('Price', 'mean'),
+        average_purchase_total_per_person=('Price', lambda x: x.sum() / num_of_players)
+    )
+
+def perform_age_purchasing_analysis(data_frame, bins, labels, num_of_players):
+    return data_frame.groupby(
+        pd.cut(data_frame['Age'], bins=bins, labels=labels)
+    ).agg(
+        purchase_count=('SN', 'count'),
+        average_purchase_price=('Price', 'mean'),
+        average_purchase_total_per_person=('Price', lambda x: x.sum() / num_of_players)
+    )
+
+def calculate_top_spenders(data_frame):
+    return data_frame.groupby('SN').agg(
+        purchase_count=('Item ID', 'count'),
+        total_purchases=('Price', 'sum'),
+        average_purchase_price=('Price', 'mean')
+    ).sort_values('total_purchases', ascending=False)
+
+def calculate_most_popular_items(data_frame):
+    return data_frame.groupby(['Item ID', 'Item Name']).agg(
+        purchase_count=('SN', 'count'),
+        total_purchase_value=('Price', 'sum'),
+        average_purchase_price=('Price', 'mean')
+    ).sort_values('purchase_count', ascending=False)
+
+def calculate_most_profitable_items(data_frame):
+    return data_frame.sort_values('total_purchase_value', ascending=False)
+
+
 # Read Purchasing File and store into Pandas DataFrame
-purchase_data = pd.read_csv("Resources/purchase_data.csv")
+purchase_data = read_purchase_data("Resources/purchase_data.csv")
 
 # Player Count
-num_of_players = purchase_data['SN'].nunique()
+num_of_players = get_num_of_players(purchase_data)
 
-# Purchasing Analysis (Total)
-summary_data_frame = pd.DataFrame({
-    'average price': purchase_data['Price'].mean(),
-    'number of unique items': purchase_data['Item Name'].nunique()
-}, index=[0])
+# Summary Data Frame
+summary_data_frame = create_summary_data_frame(purchase_data)
 
 # Gender Demographics
-gender_data = pd.DataFrame({
-    'percentage of players': purchase_data['Gender'].value_counts(normalize=True) * 100,
-    'total count': purchase_data['Gender'].value_counts()
-})
+gender_data = calculate_gender_demographics(purchase_data)
 
 # Age Demographics
-bins = [0, 10, 15, 20, 25, 30, 35, 40, 200]
-labels = ['less than 10', '10 - 14', '15 - 19', '20 - 24', '25 - 29', '30 - 34', '35 - 39', 'greater than 40']
-age_demographics = purchase_data.drop_duplicates(subset='SN').groupby(pd.cut(purchase_data['Age'], bins=bins, labels=labels)).size().reset_index(name='total count')
-age_demographics['percentage of players'] = age_demographics['total count'] / num_of_players * 100
+bins = list(range(0, 101, 5))
+labels = ['less than 10'] + [f'{i} - {i + 4}' for i in range(10, 45, 5)]
+age_demographics = calculate_age_demographics(purchase_data, bins, labels, num_of_players)
 
-# Purchasing Analysis (Gender and Age)
-purchasing_columns = {
-    'purchase count': 'SN',
-    'average purchase price': 'Price',
-    'average purchase total per person': lambda x: x.sum() / num_of_players
-}
+# Purchasing Analysis (Gender)
+gender_purchasing_analysis_df = perform_gender_purchasing_analysis(purchase_data)
 
-gender_purchasing_analysis_df = purchase_data.groupby('Gender').agg(purchasing_columns)
-age_purchasing_demo = purchase_data.groupby(pd.cut(purchase_data['Age'], bins=bins, labels=labels)).agg(purchasing_columns)
+# Purchasing Analysis (Age)
+age_purchasing_demo = perform_age_purchasing_analysis(purchase_data, bins, labels, num_of_players)
 
 # Top Spenders
-top_spenders = purchase_data.groupby('SN').agg({
-    'Item ID': 'count',
-    'Price': ['sum', 'mean']
-})
-top_spenders.columns = ['purchase count', 'total purchases', 'average purchase price']
-top_spenders = top_spenders.sort_values('total purchases', ascending=False)
+top_spenders = calculate_top_spenders(purchase_data)
 
 # Most Popular Items
-most_popular_items = purchase_data.groupby(['Item ID', 'Item Name']).agg({
-    'SN': 'count',
-    'Price': ['sum', 'mean']
-})
-most_popular_items.columns = ['purchase count', 'total purchase value', 'average purchase price']
-most_popular_items = most_popular_items.sort_values('purchase count', ascending=False)
+most_popular_items = calculate_most_popular_items(purchase_data)
 
 # Most Profitable Items
-most_profitable_items = most_popular_items.sort_values('total purchase value', ascending=False)
+most_profitable_items = calculate_most_profitable_items(most_popular_items)
